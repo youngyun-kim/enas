6d0aac87aedee98024177b56d2e1fe29b0e3ef71
youngyun.kim <youngyun.kim@snu.ac.kr>
2021-05-24 08:02:01 +0900

    [ENAS] update tc scripts

--- scripts/tc5_3_cifar10_micro_final.sh
+++ scripts/tc5_3_cifar10_micro_final.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-# name : tc5_2_cifar10_micro_final.sh
+# name : tc5_3_cifar10_micro_final.sh
 # description : 
 # copy cifar10_micro_final.sh
 # num_epochs changes from 630 to 150
 # stack_conv=2 (not changed)
-# training controller : num_epochs=130/150
-# val_acc=0.5063, cpu_latency_sum=29181.4805, gpu_latency_sum=3447.9358, reward=0.0058
+# training controller : num_epochs=150/150 
+# val_acc=0.6438, cpu_latency_sum=46511.5156, gpu_latency_sum=5900.1118, reward=0.3086
 
 export PYTHONPATH="$(pwd)"
 
-fixed_arc="0 3 1 3 0 3 2 2 0 2 1 0 4 2 2 2 0 2 0 0"
-fixed_arc="$fixed_arc 0 3 0 2 1 4 2 4 1 2 0 0 0 3 1 2 2 2 1 2"
+fixed_arc="0 4 0 1 1 4 1 4 1 2 0 0 1 4 0 2 0 2 0 1"
+fixed_arc="$fixed_arc 0 2 0 2 0 0 2 1 0 4 3 1 1 1 2 4 0 2 0 1"
 
-CUDA_VISIBLE_DEVICES=7 python src/cifar10/main.py \
+CUDA_VISIBLE_DEVICES=6 python src/cifar10/main.py \
   --data_format="NHWC" \
   --search_for="micro" \
   --reset_output_dir \
   --data_path="data/cifar-10-batches-py" \
-  --output_dir="outputs_tc5_2_cifar10_micro_final" \
+  --output_dir="outputs_tc5_3_cifar10_micro_final" \
   --batch_size=144 \
   --num_epochs=150 \
   --log_every=50 \
--- scripts/tc5_4_cifar10_micro_final.sh
+++ scripts/tc5_4_cifar10_micro_final.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-# name : tc5_2_cifar10_micro_final.sh
+# name : tc5_4_cifar10_micro_final.sh
 # description : 
 # copy cifar10_micro_final.sh
 # num_epochs changes from 630 to 150
 # stack_conv=2 (not changed)
-# training controller : num_epochs=130/150
-# val_acc=0.5063, cpu_latency_sum=29181.4805, gpu_latency_sum=3447.9358, reward=0.0058
+# training controller : num_epochs=128/150 
+# val_acc=0.6000, cpu_latency_sum=24750.7207, gpu_latency_sum=2925.7917, reward=0.3007
 
 export PYTHONPATH="$(pwd)"
 
-fixed_arc="0 3 1 3 0 3 2 2 0 2 1 0 4 2 2 2 0 2 0 0"
-fixed_arc="$fixed_arc 0 3 0 2 1 4 2 4 1 2 0 0 0 3 1 2 2 2 1 2"
+fixed_arc="0 2 1 3 1 2 0 2 1 3 1 4 0 4 1 3 0 0 1 2"
+fixed_arc="$fixed_arc 1 3 0 2 1 3 1 4 0 3 1 2 1 4 0 2 5 0 1 0"
 
-CUDA_VISIBLE_DEVICES=7 python src/cifar10/main.py \
+CUDA_VISIBLE_DEVICES=5 python src/cifar10/main.py \
   --data_format="NHWC" \
   --search_for="micro" \
   --reset_output_dir \
   --data_path="data/cifar-10-batches-py" \
-  --output_dir="outputs_tc5_2_cifar10_micro_final" \
+  --output_dir="outputs_tc5_4_cifar10_micro_final" \
   --batch_size=144 \
   --num_epochs=150 \
   --log_every=50 \
